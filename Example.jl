ae6ab47d5cfcb18510a486c875a0db781453ec8b
Tim Davis <davis@gfz-potsdam.de>
2020-04-15 12:49:20 +0200

    Working version

--- Example.jl
+++ Example.jl
@@ -1,29 +1,35 @@
-using fit
+using FitSeries
 
 function MyFunction(x)
-	r=rand(1)[1]
-	y=sin(x)+cos(x/pi)+((r-0.5)/10) #Rand nos from -0.1:0.1
+	r=rand(1)[1] #Random number between 0 and 1
+	y=sin(x)+cos(x/pi)+((r-0.5)/10) #Random noise from -0.1:0.1
 end
 
-
+n=100
 println("setup test data")
-x = range(0,stop=2*pi,length=n) #points(S,20);# the default grid
+x = range(0,stop=2*pi,length=n) 
 y = zeros(n)
 for i=1:n
-	y[i] = MyFunction(x[i]);      # values at the default grid
+	y[i] = MyFunction(x[i]);     
 end
 
-println("interpolating func")
-f=fit(x,y,"Chebyshev",5)
+println("fitting the data")
+nocoeffs=5
+fitType="Chebyshev"
+f=FitSeries.fit(x,y,fitType,nocoeffs)
 
 println("evaluating interpolated func")
 result = zeros(n)
 for i=1:n
-	result[i] = f(p[i]);      # values at the default grid
+	result[i] = f(x[i]);      
 end
 
+using Plots
 println("plotting")
 plot1 = plot()
-plot!(p,result)
-scatter!(p,v)
+scatter!(x,y,color = :lightblue,label="InputData")
+plot!(x,result,color = :black,label="Fit with $fitType, $nocoeffs coefficients")
+yaxis!("y")#, (0,1.5))
+xaxis!("x")#, (-180,180))
 display(plot1)
+#savefig(plot1)
